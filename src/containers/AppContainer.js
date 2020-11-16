4bc9cd88316b79fb43afb382420ecc11f18fd3c4
Guilherme <guilherme.pcruz@hotmail.com>
2020-11-15 22:39:20 -0300

    Atualização

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -5,19 +5,24 @@ import { Container } from 'react-bootstrap';
 import Login from "./Login";
 
 function AppContainer(props) {
+    const [page, setPage] = useState("login");
     return (
         <div className="App">
             <Container>
+            <div className="nav">
+            <ul className="menu">
+                <li className="menu-item" onClick={() => setPage("login")}>Login</li>
+                <li className="menu-item" onClick={() => setPage("resumo")}>Resumo</li>
+                <li className="menu-item" onClick={() => setPage("content")}>Conteúdo</li>
+                <li className="menu-item" onClick={() => setPage("welcome")}>Bem vindo</li>
+            </ul>
+        </div>
                 <Row>
-                    <Col>Explicação do Projeto</Col>
-                    <Col sm={6}>
-                    <Login></Login></Col>
-                </Row>
-                <Row>
-                    <Col>Conteúdo</Col>
-                </Row>
-                <Row>
-                    <Col>Boas vindas</Col>
+                    <Col>
+                    {page === "resumo" && "Explicação do Projeto"}
+                    {page === "login" && <Login/>}
+                    {page === "content" && "Conteúdo"}
+                    {page === "welcome" && "Boas vindas"}</Col>
                 </Row> 
             </Container>
         </div>
