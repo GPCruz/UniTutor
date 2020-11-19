a0f2b646d05b78632b085f4e69f6cf08e7c063b4
Guilherme <guilherme.pcruz@hotmail.com>
2020-11-19 17:06:38 -0300

    Texto do resultado refeito

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,20 +1,20 @@
 import Col from 'react-bootstrap/Col'
 import React from 'react';
 
-let nome_aluno = 'Yonarah';
-let greet_text = <>Olá {nome_aluno}!<br />
-Parece que você está com dificuldade em <b>Cálculo 3</b> e <b>Estatística</b>.<br />
-Aqui vão alguns materiais que podem ajudar com seus estudos:<br />
-Link<br />
-Link<br />
-Link<br /></>;
-
+const nome_aluno = 'Arnaldo Batista';
 
 function Welcome() {
 
-    return <>
-        <Col>{greet_text}</Col>
-    </>
+    return  <Col>
+                Olá, {nome_aluno}!<br />
+                Parece que você está com <b>muita dificuldade</b> em <b>Física 1</b>, <b>Fundamentos de Química</b>, <b>Comunicação e Expressão</b> e <b>Raciocínio Lógico</b>.<br />
+                Aqui vão alguns materiais que podem ajudar com seus estudos:<br /><br />
+                <a href="https://www.fisica.net/problemasresolvidos/">https://www.fisica.net/problemasresolvidos/</a><br />
+                <a href="https://www.todamateria.com.br/exercicios/exercicios-de-quimica/">https://www.todamateria.com.br/exercicios/exercicios-de-quimica/</a><br />
+                <a href="http://www.consultec.com.br/provas/Unifacs20141.pdf">http://www.consultec.com.br/provas/Unifacs20141.pdf</a><br />
+                <a href="https://www.estudegratis.com.br/questoes-de-concurso/materia/raciocinio-logico">https://www.estudegratis.com.br/questoes-de-concurso/materia/raciocinio-logico</a><br />
+            </Col>
+        
 }
   
 export default Welcome
